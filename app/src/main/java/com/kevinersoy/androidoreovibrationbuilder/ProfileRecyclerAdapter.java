7e784e438c6ea428e96dd803ed15f9767f414ce4
Kevin Ersoy <kevinersoy@kevinersoy.com>
2018-03-08 09:08:42 -0800

    Replaced ListView with RecyclerView.  Created view for items in the list, built the Adapter class ProfileRecyclerAdapter, and updated VibrationProfileList activity to make use of the RecyclerView.

---
+++ app/src/main/java/com/kevinersoy/androidoreovibrationbuilder/ProfileRecyclerAdapter.java
@@ -0,0 +1,72 @@
+package com.kevinersoy.androidoreovibrationbuilder;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by kevinersoy on 3/8/18.
+ */
+
+public class ProfileRecyclerAdapter extends RecyclerView.Adapter<ProfileRecyclerAdapter.ViewHolder> {
+
+    private final Context mContext;
+    private final List<ProfileInfo> mProfiles;
+    private final LayoutInflater mLayoutInflater;
+
+    public ProfileRecyclerAdapter(Context context, List<ProfileInfo> profiles) {
+        mContext = context;
+        mProfiles = profiles;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_profile_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ProfileInfo profile = mProfiles.get(position);
+        holder.mTextName.setText(profile.getName());
+        holder.mTextIntensity.setText(profile.getIntensity());
+        holder.mTextDelay.setText(profile.getDelay());
+        holder.mCurrentPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProfiles.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mTextName;
+        public final TextView mTextIntensity;
+        public final TextView mTextDelay;
+        public int mCurrentPosition;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextName = (TextView)itemView.findViewById(R.id.text_name);
+            mTextIntensity = (TextView)itemView.findViewById(R.id.text_intensity);
+            mTextDelay = (TextView)itemView.findViewById(R.id.text_delay);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mContext, VibrationProfile.class);
+                    intent.putExtra(VibrationProfile.PROFILE_POSITION, mCurrentPosition);
+                    mContext.startActivity(intent);
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/kevinersoy/androidoreovibrationbuilder/VibrationProfileList.java
+++ app/src/main/java/com/kevinersoy/androidoreovibrationbuilder/VibrationProfileList.java
@@ -4,17 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.util.List;
 
 public class VibrationProfileList extends AppCompatActivity {
-
-    private ArrayAdapter<ProfileInfo> mAdapterProfiles;
+    private ProfileRecyclerAdapter mProfileRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,27 +35,18 @@ public class VibrationProfileList extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mAdapterProfiles.notifyDataSetChanged();
+        mProfileRecyclerAdapter.notifyDataSetChanged();
     }
 
     private void initializeContent() {
-        final ListView listProfiles = (ListView)findViewById(R.id.list_profiles);
 
-        //getInstance initializes the example profiles for now
+        final RecyclerView recyclerProfiles = (RecyclerView) findViewById(R.id.list_profiles);
+        final LinearLayoutManager profilesLayoutManager = new LinearLayoutManager(this);
+        recyclerProfiles.setLayoutManager(profilesLayoutManager);
+
         List<ProfileInfo> profiles = DataManager.getInstance().getProfiles();
-        mAdapterProfiles = new ArrayAdapter<ProfileInfo>(this,android.R.layout.simple_list_item_1, profiles);
-
-        listProfiles.setAdapter(mAdapterProfiles);
-
-        listProfiles.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(VibrationProfileList.this, VibrationProfile.class);
-                intent.putExtra(VibrationProfile.PROFILE_POSITION, position);
-                startActivity(intent);
-            }
-        });
-
+        mProfileRecyclerAdapter = new ProfileRecyclerAdapter(this, profiles);
+        recyclerProfiles.setAdapter(mProfileRecyclerAdapter);
     }
 
 
